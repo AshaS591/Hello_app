b657ee658fec7886692fff215c653226c6fa44a4
AshaS591 <as1579119@gmail.com>
2025-09-26 13:34:56 +0530

    V5.1

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ print("This is a sample Python application.")
 
 
 def greet(name):
-    return f"Hello, {name}!"
+    return f"Hello, {name}! Welcome to the Python world.This message is generated using a function."
+
 print(greet("Asha"))
